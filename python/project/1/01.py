86d9b2b53fc39859ac1dd6eec67d60724543d170
JiminLee411 <wlals41189@gmail.com>
2019-07-19 18:06:45 +0900

    dskfh

---
+++ python/project/1/01.py
@@ -0,0 +1,43 @@
+import requests
+import pprint
+import csv
+from datetime import datetime, timedelta
+
+key = '896e0630e1c45d2f0cd6b6976b1e6391'
+targetDt = '20190713' # yyyymmdd
+week = '0'
+per = {}
+total = {}
+for i in range(50):
+    api_url = f'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key={key}&targetDt={targetDt}&weekGb={week}'
+    print(api_url)
+    response = requests.get(api_url).json()
+    targetDt = datetime.strptime(targetDt, '%Y%m%d').date()
+    targetDt += timedelta(days=-7)
+    targetDt = targetDt.strftime('%Y%m%d')
+
+#     for j in range(10):
+#         if response['boxOfficeResult']['weeklyBoxOfficeList'][j]['movieCd'] not in movieCd:
+#             audiAcc.append(int(response['boxOfficeResult']['weeklyBoxOfficeList'][j]['audiAcc']))
+#             movieCd.append(response['boxOfficeResult']['weeklyBoxOfficeList'][j]['movieCd'])
+#             movieNm.append(response['boxOfficeResult']['weeklyBoxOfficeList'][j]['movieNm'])
+#             per['영화제목'] = movieNm[-1]
+#             per['영화코드'] = movieCd[-1]
+#             per['누적순위'] = audiAcc[-1]            
+#             total[(i+1)*j] = per
+
+    for movie in response['boxOfficeResult']['weeklyBoxOfficeList']:
+        if movie['movieCd'] not in total.keys():
+            total[movie['movieCd']] = {
+                'movieCd': movie['movieCd'],
+                'movieNm': movie['movieNm'],
+                'audiAcc': movie['audiAcc']
+            }
+print(total)
+
+with open('boxoffice.csv', 'w', encoding='utf-8') as f:
+    fieldnames = ['movieCd', 'movieNm', 'audiAcc']
+    csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
+    csv_writer.writeheader()
+    for item in total.values():
+        csv_writer.writerow(item)
