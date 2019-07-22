2cd912b00e16ceadadaa277b4d42d261529585e3
JiminLee411 <wlals41189@gmail.com>
2019-07-22 18:00:04 +0900

    20190722 | python Day 6 | module 및 data_structure

---
+++ python/notes/my_math.py
@@ -0,0 +1,13 @@
+PI = 3.141592
+E = 2.17
+
+def sum(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+if __name__ == '__main__':
+    print('직접호출함')
+    print(__name__)
+    sum(3, 5)
