3bc54e9cc419922c7546829c350b2113297606b0
JiminLee411 <wlals41189@gmail.com>
2019-07-25 17:52:43 +0900

    190725 | python - day9 | OOP_advanced

--- python/project/1/basic.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-key = '896e0630e1c45d2f0cd6b6976b1e6391'
-targetDt = '20190713' # yyyymmdd
-api_url = f'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key={key}&targetDt={targetDt}'
-print(api_url)
-response = requests.get(api_url).json()
-print(response)
